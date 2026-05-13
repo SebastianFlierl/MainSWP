6d9c2b7e2b7fbd0b445fd564a51dd197f5c0a8f4
SebastianFlierl <sebastian.flierl@student.htldornbirn.at>
2026-05-13 13:54:22 +0200

    Fast fertig

--- Simpson/Simpson.js
+++ Simpson/Simpson.js
@@ -1,47 +1,75 @@
-var figuren = ["Apu","Barney","Bart","Charles","Glancy","Grandpa","Hibbert","Homer","Krusty","Marge"];
+// Simpson.js
+
+var figuren = [
+    "apu",
+    "barney",
+    "bart",
+    "charles",
+    "glancy",
+    "grandpa",
+    "hibbert",
+    "homer",
+    "krusty",
+    "marge"
+];
+
 
 function changePics() {
-    let bilder = document.querySelectorAll(".place img");
-    let namenFelder = document.querySelectorAll("[id^='name']");
 
-    // Bilder + Namen setzen
-    for (let i = 0; i < bilder.length; i++) {
-        bilder[i].src = "bilder/" + figuren[i] + ".png";
+    // Alle DIVs mit class="place"
+    var places = document.querySelectorAll(".place");
 
-        // Name darunter (Vorname groß)
-        let name = figuren[i];
-        namenFelder[i].innerHTML = name.charAt(0).toUpperCase() + name.slice(1);
+    // Neue Klasse hinzufügen,
+    // aber nur wenn KEIN id-Attribut vorhanden ist
+    places.forEach(function(place) {
+        if (!place.hasAttribute("id")) {
+            place.classList.add("yellowplace");
+        }
+    });
 
-        // Klick-Event
-        bilder[i].addEventListener("click", function() {
-            toggleImages(this);
-        });
+    // Hintergrundfarben ändern
+    document.getElementById("rowone").style.background = "orange";
+    document.getElementById("rowtwo").style.background = "blue";
+
+    // Alle Bilder holen
+    var bilder = document.querySelectorAll(".place img");
+
+    // Bilder + Namen einsetzen
+    for (var i = 0; i < bilder.length; i++) {
+
+        // Bildpfad mit Nummer erzeugen
+        bilder[i].src = "bilder/" + figuren[i] + "_" + (i + 1) + ".png";;
+
+        // Namen unter die Bilder schreiben
+        var namefeld = document.getElementById("name" + (i + 1));
+        namefeld.innerHTML = figuren[i];
+
+        // Klickfunktion
+        bilder[i].onclick = function () {
+
+            // Prüfen ob andere Bilder sichtbar sind
+            var andereSichtbar = false;
+
+            bilder.forEach(function(img) {
+                if (img !== this && img.style.visibility !== "hidden") {
+                    andereSichtbar = true;
+                }
+            }, this);
+
+            // Bilder ein-/ausblenden
+            bilder.forEach(function(img) {
+
+                if (img !== this) {
+
+                    if (andereSichtbar) {
+                        img.style.visibility = "hidden";
+                    } else {
+                        img.style.visibility = "visible";
+                    }
+
+                }
+
+            }, this);
+        };
     }
-
-    // Klasse für alle .place ohne id hinzufügen
-    let places = document.querySelectorAll(".place");
-    places.forEach(div => {
-        if (!div.hasAttribute("id")) {
-            div.classList.add("noid");
-        }
-    });
-}
-
-// Bilder ein-/ausblenden
-function toggleImages(clickedImg) {
-    let alleBilder = document.querySelectorAll(".place img");
-
-    let hidden = false;
-
-    alleBilder.forEach(img => {
-        if (img !== clickedImg && img.style.display === "none") {
-            hidden = true;
-        }
-    });
-
-    alleBilder.forEach(img => {
-        if (img !== clickedImg) {
-            img.style.display = hidden ? "block" : "none";
-        }
-    });
 }
